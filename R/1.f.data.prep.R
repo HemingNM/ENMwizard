7d82d8fef0cd58c0f1502bd2520d8cb5b3d92499
HemingNM <neanderh@yahoo.com.br>
2017-09-26 15:17:39 -0300

    update

--- R/1.f.data.prep.R
+++ R/1.f.data.prep.R
@@ -15,28 +15,25 @@ f.poly <- function(occ.spdf, o.path = NULL, lr.nm="occ_poly", convex=T, alpha=10
   if(convex==F){ # convex hulls to crop rasters
     # http://r.789695.n4.nabble.com/Concave-hull-td863710.html#a4688606
     # https://rpubs.com/geospacedman/alphasimple
-    library(alphahull)
-    library(igraph)
-    library(sp)
 
-    ch <- ashape(unique(coordinates(occ.spdf)), alpha=10)
-    chg = graph.edgelist(cbind(as.character(ch$edges[, "ind1"]),
+    ch <- alphahull::ashape(unique(coordinates(occ.spdf)), alpha=10)
+    chg <- igraph::graph.edgelist(cbind(as.character(ch$edges[, "ind1"]),
                                as.character(ch$edges[, "ind2"])), directed = FALSE)
-    if (!is.connected(chg)) {
+    if (!igraph::is.connected(chg)) {
       stop("Graph not connected")
-    } else if (any(degree(chg) != 2)) {
+    } else if (any(igraph::degree(chg) != 2)) {
       stop("Graph not circular")
-    } else if (clusters(chg)$no > 1) {
+    } else if (igraph::clusters(chg)$no > 1) {
       stop("Graph composed of more than one circle")
     }
     # In the next step, we delete one edge of the circle to create a chain,
     # and find the path from one end of the chain to the other.
-    cutg <- chg - E(chg)[1]
+    cutg <- chg - igraph::E(chg)[1]
     # find chain end points
-    ends <- names(which(degree(cutg) == 1))
-    path <- get.shortest.paths(cutg, ends[1], ends[2])[[1]]
+    ends <- names(which(igraph::degree(cutg) == 1))
+    path <- igraph::get.shortest.paths(cutg, ends[1], ends[2])[[1]]
     # this is an index into the points
-    pathX <- as.numeric(V(chg)[path[[1]]]$name)
+    pathX <- as.numeric(igraph::V(chg)[path[[1]]]$name)
     # join the ends
     pathX <- c(pathX, pathX[1])
     # now show the alpha shape plot with our poly on top
@@ -45,14 +42,14 @@ f.poly <- function(occ.spdf, o.path = NULL, lr.nm="occ_poly", convex=T, alpha=10
     # lines(ch$x[pathX, ], lwd = 2)
     coords <- ch$x[pathX, ]
   } else {
-    ch <- chull(coordinates(occ.spdf))
-    coords <- coordinates(occ.spdf)[c(ch, ch[1]),]
+    ch <- grDevices::chull(coordinates(occ.spdf))
+    coords <- sp::coordinates(occ.spdf)[c(ch, ch[1]),]
   }
-  occ_poly <- SpatialPolygons(list(Polygons(list(Polygon(coords)), ID=1)))
-  occ_poly <- SpatialPolygonsDataFrame(occ_poly, data=data.frame(ID=1))
+  occ_poly <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(coords)), ID=1)))
+  occ_poly <- sp::SpatialPolygonsDataFrame(occ_poly, data=data.frame(ID=1))
   crs(occ_poly) <- crs.set
   if(!is.null(o.path)){
-    shapefile(occ_poly, filename = paste(o.path, paste0(lr.nm,".shp"), sep = "/" ), overwrite=TRUE)
+    raster::shapefile(occ_poly, filename = paste(o.path, paste0(lr.nm,".shp"), sep = "/" ), overwrite=TRUE)
   }
   return(occ_poly)
 }
@@ -72,8 +69,8 @@ f.poly.batch <- function(spp.occ.list, o.path=NULL, crs.set = NA, convex=T, alph
   if(!is.null(o.path)) {if(dir.exists(o.path)==F) dir.create(o.path)}
   for(i in 1:length(spp.occ.list)){
     occ.spdf <- data.frame(spp.occ.list[[i]])
-    coordinates(occ.spdf) <- ~LONG+LAT
-    crs(occ.spdf) <- crs.set
+    sp::coordinates(occ.spdf) <- ~LONG+LAT
+    raster::crs(occ.spdf) <- crs.set
     occ.pgns[[i]] <- f.poly(occ.spdf, o.path=o.path, lr.nm=lr.nm[i], convex=convex, alpha=alpha, crs.set=crs.set)
     plot(occ.pgns[[i]], main=names(spp.occ.list)[i])
     plot(occ.spdf, col="red", add=T)
@@ -86,6 +83,18 @@ f.poly.batch <- function(spp.occ.list, o.path=NULL, crs.set = NA, convex=T, alph
 
 
 #### 1.2 creating buffer
+#' Create buffer based on polygon
+#'
+#' @param occ_polys Spatial polygon
+#' @param bffr.width Buffer width. See 'width' of ?gBuffer
+#' @param mult How much expand bffr.width
+#' @param quadsegs
+#' @param o.path
+#' @param crs.set
+#' @param plot Boolean, to draw plots or not
+#' @return A named list of SpatialPolygons
+#' @examples
+#' occ_b <- f.bffr(occ_polys, bffr.width=1.5, crs.set=crs.set) #
 f.bffr <- function(occ_polys, bffr.width=NULL, mult=.2, quadsegs=100, o.path = "occ_poly", crs.set=NULL, plot=T){
   # https://gis.stackexchange.com/questions/194848/creating-outside-only-buffer-around-polygon-using-r
   library(rgeos)
@@ -98,31 +107,25 @@ f.bffr <- function(occ_polys, bffr.width=NULL, mult=.2, quadsegs=100, o.path = "
   if(length(mult)==1){ mult <- rep(mult, length(occ_polys))}
   TF.b.w <- is.null(bffr.width)
   for(i in 1:length(occ_polys)){
-    if(!is.null(crs.set) & is.null(projection(occ_polys[[i]]))){projection(occ_polys[[i]]) <- crs.set}
+    if(!is.null(crs.set) & is.null(raster::projection(occ_polys[[i]]))){raster::projection(occ_polys[[i]]) <- crs.set}
     if(TF.b.w){
-      ext_proj <- extent(occ_polys[[i]])
+      ext_proj <- raster::extent(occ_polys[[i]])
       bffr.width <- mean(c(abs(ext_proj[1]-ext_proj[2]), abs(ext_proj[3]-ext_proj[4])))*mult[i]
     }
     cat(c("Buffer width for", names(occ_b)[i], "is", bffr.width, "\n"))
-    occ_b[[i]] <- gBuffer(occ_polys[[i]], width=bffr.width, quadsegs=quadsegs)
+    occ_b[[i]] <- rgeos::gBuffer(occ_polys[[i]], width=bffr.width, quadsegs=quadsegs)
     crs(occ_b[[i]]) <- crs.set
     shapefile(occ_b[[i]], filename = paste(o.path, "bffr", paste0(names(occ_b)[i], "_bffr", ".shp"), sep = "/" ), overwrite=TRUE)
-    occ_b[[i]] <- shapefile(paste(o.path, "bffr", paste0(names(occ_b)[i], "_bffr", ".shp"), sep = "/" ))
+    occ_b[[i]] <- raster::shapefile(paste(o.path, "bffr", paste0(names(occ_b)[i], "_bffr", ".shp"), sep = "/" ))
 
     if(plot == T){
       plot(occ_b[[i]], col="green", main=names(occ_b)[i])
       plot(occ_polys[[i]], border="red", add=T)
-      # occ.spdf <- data.frame(spp.occ.list[[i]])
-      # coordinates(occ.spdf) <- ~LONG+LAT
-      # crs(occ.spdf) <- crs.set
-      # plot(occ.spdf, col="red", add=T)
-      # plot(NewWorld, add=T)
     }
   }
   return(occ_b)
 }
 
-# occ_b <- f.bffr(occ_polys, bffr.width=1.5, crs.set=crs.set) #
 
 
 # # path to environmental variables
