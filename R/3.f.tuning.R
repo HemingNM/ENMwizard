0c8cdfaadbb14253d962914bb08667f69b77c49b
HemingNM <neanderh@yahoo.com.br>
2017-12-18 17:37:24 -0200

    added method = "block" in ENMevaluate.batch

--- R/3.f.tuning.R
+++ R/3.f.tuning.R
@@ -16,7 +16,7 @@
 ENMevaluate.batch <- function(occ.l, a.calib.l, bg.coords.l = NULL, occ.grp = NULL,
                               bg.grp = NULL, RMvalues = seq(0.5, 4, 0.5),
                               fc = c("L", "LQ", "H", "LQH", "LQHP", "LQHPT"),
-                              categoricals = NULL, n.bg = 10000, method = NULL,
+                              categoricals = NULL, n.bg = 10000, method = "block",
                               overlap = FALSE, aggregation.factor = c(2, 2),
                               kfolds = NA, bin.output = FALSE, clamp = TRUE,
                               rasterPreds = TRUE, parallel = FALSE, numCores = NULL,
