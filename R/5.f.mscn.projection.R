43b8ddb98ceafa38418d162e90c9dc7d4d59008a
HemingNM <neanderh@yahoo.com.br>
2017-12-21 11:07:29 -0200

    5.f.mscn.projection: mxnt.p: added item list (mxnt.c.mdls$mxnt.preds) to organize projections

--- R/5.f.mscn.projection.R
+++ R/5.f.mscn.projection.R
@@ -130,7 +130,13 @@ mxnt.p <- function(mxnt.c.mdls, sp.nm, pred.nm="fut", a.proj, formt = "raster",n
     }
   }
 
-  mxnt.c.mdls <- append(mxnt.c.mdls, stats::setNames(list(mod.preds) , paste0("mxnt.preds", pred.nm)))
+  # mxnt.c.mdls <- append(mxnt.c.mdls, stats::setNames(list(mod.preds) , paste0("mxnt.preds", pred.nm)))
+  if(is.null(mxnt.c.mdls$mxnt.preds)){
+    mxnt.c.mdls$mxnt.preds <- stats::setNames(list(mod.preds) , paste0(pred.nm))
+  } else {
+    mxnt.c.mdls$mxnt.preds <- append(mxnt.c.mdls$mxnt.preds, stats::setNames(list(mod.preds) , paste0(pred.nm)))
+  }
+
   return(mxnt.c.mdls)
 }
 
