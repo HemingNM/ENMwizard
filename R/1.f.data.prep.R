56e55babb0b7b68f32a5d7e56475576a2e797cb9
HemingNM <neanderh@yahoo.com.br>
2018-03-03 07:31:31 -0300

    bug fix in poly.splt

--- R/1.f.data.prep.R
+++ R/1.f.data.prep.R
@@ -19,7 +19,7 @@ poly.c <- function(occ.spdf, sp.nm="sp.nm", convex=T, alpha=10, save=T, crs.set 
   if(dir.exists("1_sppData")==F) dir.create("1_sppData")
   if(dir.exists(o.path)==F) dir.create(o.path)
 
-  u.pts <- unique(sp::coordinates(occ.spdf))
+  u.pts <- as.data.frame(unique(sp::coordinates(occ.spdf)))
 
   if(convex==F){ # convex hulls to crop rasters
     # http://r.789695.n4.nabble.com/Concave-hull-td863710.html#a4688606
@@ -225,7 +225,7 @@ poly.splt <- function(occ.spdf, k=NULL, c.m = "NB", r = 2, q = 0.3,
                       crs.set = "+proj=longlat +datum=WGS84"){ # , o.path = "occ.poly"
 
   # u.pts <- sp::coordinates(occ.spdf)
-  u.pts <- unique(sp::coordinates(occ.spdf))
+  u.pts <- as.data.frame(unique(sp::coordinates(occ.spdf)))
 
   if(k == 0 | is.null(k)){
     # http://www.sthda.com/english/articles/29-cluster-validation-essentials/96-determining-the-optimal-number-of-clusters-3-must-know-methods/
@@ -265,6 +265,12 @@ poly.splt <- function(occ.spdf, k=NULL, c.m = "NB", r = 2, q = 0.3,
   }
 
   # create one polygon for each set of points
+  sp::coordinates(u.pts) <- cbind(u.pts[,1], u.pts[,2])
+    # if(!class(u.pts) %in% c("SpatialPoints", "SpatialPointsDataFrame")){
+    #   lon.col <- colnames(u.pts)[grep("^lon$|^long$|^longitude$", colnames(u.pts), ignore.case = T, fixed = F)][1]
+    #   lat.col <- colnames(u.pts)[grep("^lat$|^latitude$", colnames(u.pts), ignore.case = T)][1]
+    #   sp::coordinates(u.pts) <- c(lon.col, lat.col)
+    # }
   spp.k.list <- lapply(1:k, function(i){u.pts[clust==i,]})
   names(spp.k.list) <- paste0(sp.nm, seq_along(spp.k.list))
   occ.polys.lst <- poly.c.batch(spp.k.list, k=1, convex=convex, alpha=alpha, plot=F, save=F)
