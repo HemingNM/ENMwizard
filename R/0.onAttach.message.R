c62ff0695c27fbfbce5a03a72db49a5e26035e88
HemingNM <neanderh@yahoo.com.br>
2018-09-12 17:37:32 -0300

    Changed .onAttach to .packageStartupMessage

--- R/0.onAttach.message.R
+++ R/0.onAttach.message.R
@@ -1,4 +1,4 @@
-.onAttach <- function(libname = find.package("ENMwizard"), pkgname = "ENMwizard") {
+.packageStartupMessage <- function(libname = find.package("ENMwizard"), pkgname = "ENMwizard") {
   packageStartupMessage(cat("    Welcome to ENMwizard \n  To cite this package use: citation('ENMwizard'). \n  Please, remember to cite other packages that ENMwizard depends on: 'ENMeval', 'spThin', 'raster'"))
 }
 
