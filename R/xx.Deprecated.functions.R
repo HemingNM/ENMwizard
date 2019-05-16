1a5584233f1ca2005dccddaa8f0acc709c7fec38
HemingNM <neanderh@yahoo.com.br>
2019-05-16 10:57:28 -0300

    Updated function names sel_env -> select_vars

---
+++ R/xx.Deprecated.functions.R
@@ -0,0 +1,13 @@
+#' Find, optionally remove, highly correlated variables from a list (several species) of raster brick/stack
+#'
+#' @export
+sel_env_b <- function(...){
+  stop("Deprecated function. Use 'select_vars_b'")
+}
+
+#' Find, optionally remove, highly correlated variables from a raster brick/stack
+#'
+#' @export
+sel_env <- function(...){
+  stop("Deprecated function. Use 'select_vars'")
+}
