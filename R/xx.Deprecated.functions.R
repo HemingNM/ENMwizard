3896d4b93af850f86648326e8d0c94a4b32dd90d
HemingNM <neanderh@yahoo.com.br>
2019-06-26 09:28:01 -0300

    Updated descriptions of deprecated functions

--- R/xx.Deprecated.functions.R
+++ R/xx.Deprecated.functions.R
@@ -1,6 +1,7 @@
 ##### 1.1.
 #' Create minimum convex polygon based on species occurence data
 #'
+#' Deprecated function. Use 'set_calibarea'
 #' This function will create minimum convex polygon based on coordinates of species occurence data.
 #'
 #' @param ... additional arguments
@@ -11,6 +12,7 @@ poly.c <- function(...){
 
 #' Create minimum convex polygon based on coordinates of species occurence data for several species
 #'
+#' Deprecated function. Use 'set_calibarea_b'
 #' This function will use a list of coordinates of species occurence data and create minimum convex polygons
 #' for each element in the list.
 #' It is possible to create concave or convex polygons, create several small polygons based on clusters
@@ -27,6 +29,7 @@ poly.c.batch <- function(...){
 
 #' Create buffer based on species polygons
 #'
+#' Deprecated function. Use 'buffer_b'
 #' This funcion will create a buffer using species polygons. Buffer width may be manually specified or
 #' calculated based on the extent of the SpatialPolygons object (i.e. mean of latitudinal and longitudinal extent).
 #' If width is calculated based on the extent of the SpatialPolygons object, it can be adjusted (enlarged or reduced)
@@ -41,7 +44,8 @@ bffr.batch <- function(...){
 
 
 #' Cut calibration area based on a list of SpatialPolygons
-## #' Crop environmental variables for each species
+#'
+#' Deprecated function. Use 'cut_calibarea_b'
 #' Use a list of SpatialPolygons to crop environmental variables for each species.
 #'
 #' @inheritParams poly.c
@@ -53,6 +57,7 @@ env.cut <- function(...){
 
 #' Spatially thin a list of species occurrence data
 #'
+#' Deprecated function. Use 'thin_b'
 #' Will use spThin optimisation algorithm to subset the dataset such that
 #' all occurrence locations are a minimum distance apart. This process helps
 #' reduce the effect of biases in observation records on the predictive
@@ -70,6 +75,7 @@ thin.batch <- function(...){
 
 #' Load filtered occurrence data
 #'
+#' Deprecated function. Use 'load_thin_occ'
 #' Load filtered occurrence data from object returned by "thin_b" function
 #'
 #' @inheritParams poly.c
@@ -84,6 +90,7 @@ loadTocc <- function(...){
 
 #' Find, optionally remove, highly correlated variables from a list (several species) of raster brick/stack
 #'
+#' Deprecated function. Use 'select_vars_b'
 #' @inheritParams poly.c
 #' @export
 sel_env_b <- function(...){
@@ -92,6 +99,7 @@ sel_env_b <- function(...){
 
 #' Find, optionally remove, highly correlated variables from a raster brick/stack
 #'
+#' Deprecated function. Use 'select_vars'
 #' @inheritParams poly.c
 #' @export
 sel_env <- function(...){
@@ -102,6 +110,7 @@ sel_env <- function(...){
 ##### 2.
 #' Select area for projection based on the extent of occ points
 #'
+#' Deprecated function. Use 'set_calibarea'
 #' This function will create SpatialPolygons that will be used to crop/mask raster/brick objects to be used on model projections. It has several options
 #' The user can crop a squared area with an extent larger than the extent of occ.poly. By default, the "extent increase"
 #' is the maximum of latitudinal and longitudinal extent "max(abs(ext.proj[1]-ext.proj[2]), abs(ext.proj[3]-ext.proj[4]))".
@@ -121,6 +130,7 @@ pred.a.poly <- function(...){
 
 #' Select area for projection based on the extent of occ points for multiple species
 #'
+#' Deprecated function. Use 'set_projarea_b'
 #' This function is a wrapper for "cut_projarea". See ?cut_projarea
 #' It works with a named list of occ.polys to delimit the projection area for each of the species.
 #'
@@ -134,6 +144,7 @@ pred.a.poly.batch <- function(...){
 
 #' Cut area for projection based on a list of SpatialPolygons
 #'
+#' Deprecated function. Use 'cut_projarea_b'
 #' This function is a wrapper for "cut_projarea". See ?cut_projarea
 #' It works with a named list of pred.poly to delimit the projection area for each of the species.
 #'
@@ -145,6 +156,7 @@ pred.a <- function(...){
 
 #' Cut area for projection based on a list of SpatialPolygons
 #'
+#' Deprecated function. Use 'cut_projarea_b'
 #' This function is a wrapper for "cut_projarea". See ?cut_projarea
 #' It works with a named list of pred.poly to delimit the projection area for each of the species.
 #'
@@ -156,6 +168,7 @@ pred.a.batch <- function(...){
 
 #' Cut multiple projection areas (climatic scenarios) for multiple species (list of SpatialPolygons)
 #'
+#' Deprecated function. Use 'cut_projarea_mscn_b'
 #' This function is a wrapper for "cut_projarea". See ?cut_projarea. This function delimits the projection area for
 #' each of the species contained in the pred.polys named list and crops
 #' multiple rasters/bricks (i.e. representing distinct climatic scenaries) based on the same criteria for each species.
@@ -167,6 +180,7 @@ pred.a.batch.mscn <- function(...){
 
 #' Cut a projection area based on a SpatialPolygon (e.g. Ecoregion)
 #'
+#' Deprecated function. Use 'cut_projarea_rst'
 #' This function will use a single SpatialPolygon to crop/mask raster/brick objects to be used on model projections.
 #'
 #' @inheritParams poly.c
@@ -177,6 +191,7 @@ pred.a.rst <- function(...){
 
 #' Cut projection areas of multiple species based on a single SpatialPolygon object (e.g. Ecoregion)
 #'
+#' Deprecated function. Use 'cut_projarea_rst_b'
 #' This function will use a single SpatialPolygon to crop/mask raster/brick objects to be used on model projections.
 #'
 #' @inheritParams poly.c
@@ -187,6 +202,7 @@ pred.a.batch.rst <- function(...){
 
 #' Cut multiple projection areas of multiple species based on a single SpatialPolygon object (e.g. Ecoregion)
 #'
+#' Deprecated function. Use 'cut_projarea_rst_mscn_b'
 #' This function will use a single SpatialPolygon to crop/mask multiple raster/brick objects
 #' to be used on model projections.
 #'
@@ -201,6 +217,7 @@ pred.a.batch.rst.mscn <- function(...){
 ##### 3.
 #' Tuning and evaluation of ENMs with Maxent for several species using ENMeval
 #'
+#' Deprecated function. Use 'ENMevaluate_b'
 #' This function is a wrapper for ENMeval::ENMevaluate. See ?ENMeval::ENMevaluate for details
 #' It works with a named list of species occurrence data (occ.l) and a list of
 #' cropped environmental variables (a.calib.l) for model tuning.
@@ -215,6 +232,7 @@ ENMevaluate.batch <- function(...){
 
 #' Optimize the size of ENMevaluate_b objects
 #'
+#' Deprecated function. Use 'optENMevalObjL'
 #' This function will set to NULL (erase) the largest slots ('predictions', 'models', 'occ.grp', and 'bg.grp')
 #' of ENMeval::ENMevaluate objects. Only results, occ.pts, and bg.pts are returned.
 #' Use with care. It will not be possible to check MaxEnte models, predictions and grouping of occ and bg points.
@@ -232,6 +250,7 @@ ENMevaluate.l.opt <- function(...){
 ## 4.2 Generate final models for occ
 #' Model selection and creation of MaxEnt arguments for selected models
 #'
+#' Deprecated function. Use 'mod_sel'
 #' This function will read an object of class ENMevaluation (See ?ENMeval::ENMevaluate for details) and
 #' return the results table with models selected by the chosen criteria. It can also return the
 #' necessary arguments for final model calibration and predictions.
@@ -247,6 +266,7 @@ f.args <- function(...){
 # "f.mxnt.mdl.pred" renamed to "calib_mdl" and now to "calib_mdl"
 #' Calibrate MaxEnt models based on model selection criteria
 #'
+#' Deprecated function. Use 'calib_mdl'
 #' This function will read an object of class ENMevaluation (See ?ENMeval::ENMevaluate for details) and
 #' calibrate the selected maxent models.
 #'
@@ -261,6 +281,7 @@ mxnt.c <- function(...){
 
 #' Calibrate MaxEnt models based on model selection criteria for several species
 #'
+#' Deprecated function. Use 'calib_mdl_b'
 #' This function will read a list of objects of class ENMevaluation (See ?ENMeval::ENMevaluate for details) and
 #' return selected maxent model calibrations and predictions. Each element on the list is usually a species.
 #' a.proj.l, a.calib.l, occ.l are lists with occurence data, projection and calibration/predictor data.
@@ -279,6 +300,7 @@ mxnt.c.batch <- function(...){
 
 #' Project calibrated MaxEnt models
 #'
+#' Deprecated function. Use 'proj_mdl'
 #' This function will read an object returned by "calib_mdl", read the calibrated models and project into
 #' new areas/climatic scenarios. These new projections will be returned together with (appended to)
 #' the original object.
@@ -291,6 +313,7 @@ mxnt.p <- function(...){
 
 #' Project calibrated MaxEnt models
 #'
+#' Deprecated function. Use 'proj_mdl'
 #' This function will read an object returned by "calib_mdl", read the calibrated models and project into
 #' new areas/climatic scenarios. These new projections will be returned together with (appended to)
 #' the original object.
@@ -303,6 +326,7 @@ mxnt.p.batch <- function(...){
 
 #' Project calibrated MaxEnt models for several species onto multiple environmental scenarios
 #'
+#' Deprecated function. Use 'proj_mdl_b'
 #' This function will read an object returned by "calib_mdl_b", read the calibrated models and project into
 #' several environmental (areas/climatic) scenarios (specified in a.proj.l). These new projections will be returned together with (appended to)
 #' each element (species) the original object.
@@ -322,6 +346,7 @@ mxnt.p.batch.mscn <- function(...){
 
 #' Apply threshold for MaxEnt projections of a species
 #'
+#' Deprecated function. Use 'thrshld'
 #' This function will apply the selected threshold criterias to MaxEnt model projection(s) of a 'mcmp' object
 #' and save on the folder "3_out.MaxEnt/Mdls.[species name]/Mdls.thrshld". For each projection (species and climatic
 #' scenario), two layers will be generated, one with suitability above the threshold value and another with presence/absence only.
@@ -336,6 +361,7 @@ f.thr <- function(...){
 #### 4.8.5 threshold for past and future pred
 #' Apply threshold for MaxEnt projections for multiple species
 #'
+#' Deprecated function. Use 'thrshld_b'
 #' This function will apply the selected threshold criterias to MaxEnt model projection(s) of a 'mcmp.l' object
 #' and save on the folder "3_out.MaxEnt/Mdls.[species name]/Mdls.thrshld". For each projection (species and climatic
 #' scenario), two layers will be generated, one with suitability above the threshold value and another with presence/absence only.
@@ -352,6 +378,7 @@ f.thr.batch <- function(...){
 #### 4.8.6 plot prediction diff between models
 #' Plot differences in suitable areas between models selected using distinct criteria for multiple species
 #'
+#' Deprecated function. Use 'plot_mdl_diff_b'
 #' Plot differences between predictions of models selected using distinct
 #' criteria (e.g. "AvgAIC", "LowAIC", "OR", "AUC") for multiple climatic scenarios and multiple species
 #'
@@ -369,6 +396,7 @@ plotMdlDiff <- function(...){
 #### 4.8.6 plot prediction diff between models
 #' Plot differences in suitable areas between models selected using distinct criteria
 #'
+#' Deprecated function. Use 'set_calibarea'
 #' Plot differences between predictions of models selected using distinct
 #' criteria (e.g. "AvgAIC", "LowAIC", "OR", "AUC") for multiple climatic scenarios
 #'
@@ -384,6 +412,7 @@ plotMdlDiff <- function(...){
 ####
 #' Plot differences in suitable areas between climatic scenarios for multiple species
 #'
+#' Deprecated function. Use 'plot_scn_diff_b'
 #' Plot differences between a selected climatic scenario and all other climatic scenarios for each species.
 #' This function will plota and (optionally) save the figures on pdf files in the folder "Mdls.thrshld/figs".
 #' @inheritParams poly.c
@@ -396,6 +425,7 @@ plotScnDiff <- function(...){
 
 #' Plot differences in suitable areas between climatic scenarios
 #'
+#' Deprecated function. Use 'set_calibarea'
 #' Plot differences between a selected climatic scenario and all other climatic scenarios for each species.
 #' This function will plota and (optionally) save the figures on pdf files in the folder "Mdls.thrshld/figs".
 #'
@@ -411,6 +441,7 @@ plotScnDiff <- function(...){
 
 #' Compute species' total suitable area
 #'
+#' Deprecated function. Use 'get_tsa_b' or 'get_tsa'
 #' Compute total suitable area at multiple climatic scenario, threshold and model criteria.
 #'
 #' @inheritParams poly.c
@@ -422,6 +453,7 @@ f.area.occ.mscn <- function(...){
 
 #' Compute species' total suitable area
 #'
+#' Deprecated function. Use 'set_calibarea'
 #' Compute total suitable area at multiple climatic scenario, threshold and model criteria.
 #'
 # #' @export
@@ -435,6 +467,7 @@ f.area.occ.mscn <- function(...){
 
 #' Compute variable contribution and permutation importance
 #'
+#' Deprecated function. Use 'get_cont_permimport_b' or 'get_cont_permimport'
 #' Compute variable contribution and importance for each model
 #'
 #' @inheritParams poly.c
@@ -446,6 +479,7 @@ f.var.ci <- function(...){
 
 #' Compute variable contribution and permutation importance
 #'
+#' Deprecated function. Use 'set_calibarea'
 #' Compute variable contribution and importance for each model
 #'
 # #' @export
@@ -456,6 +490,7 @@ f.var.ci <- function(...){
 
 #' Compute "Fractional predicted area" ('n of occupied pixels'/n)
 #'
+#' Deprecated function. Use 'get_fpa_b' or 'get_fpa'
 #' Compute "Fractional predicted area" ('n of occupied pixels'/total n) or ('area of occupied pixels'/total area)
 #'
 #' @inheritParams poly.c
@@ -476,6 +511,7 @@ f.FPA <- function(...){
 
 #' Compute "Omission Rate"
 #'
+#' Deprecated function. Use 'get_OR' or 'get_or_ensemble' or 'get_or_ensemble_b'
 #' Compute "Omission Rate" of species occurence points for a climatic scenario (usually "current")
 #'
 #' @inheritParams poly.c
@@ -539,13 +575,15 @@ f.OR <- function(...){
 # #### 5.3 comparar as distribuições (rasteres de adequabilidade) geradas por diferentes critérios de
 # ## seleção de modelo (AvgAIC, LowAIC, avg.test.orMTP, avg.test.or10pct, avg.test.AUC.MTP, avg.test.AUC10pct),
 # ## usando função ENMTools::raster.overlap(r1, r2).
+
 #' Raster overlap between models selected using different criteria
 #'
+#' Deprecated function. Use 'raster_overlap_b'
 #' Measures overlap between two ENMs. Used to compare differences among model selection criteria.
 #' See ?ENMTools::raster.overlap for details.
 #'
-# #' @inheritParams poly.c
-# #' @export
-# f.raster.overlap.mscn <- function(...){
-#   stop("Deprecated function. Use 'raster_overlap_b'")
-# }
+#' @inheritParams poly.c
+#' @export
+f.raster.overlap.mscn <- function(...){
+  stop("Deprecated function. Use 'raster_overlap_b'")
+}
