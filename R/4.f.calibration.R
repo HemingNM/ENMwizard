597073fcb75ce6db689655e584e9437f7033ee3a
HemingNM <neanderh@yahoo.com.br>
2021-01-25 15:20:48 -0200

    remove NAs from ESORIC delta AIC calculation

--- R/4.f.calibration.R
+++ R/4.f.calibration.R
@@ -84,24 +84,24 @@ mod_sel <- function(x, mSel=c("AvgAIC", "EBPM", "WAAUC", "ESORIC", "LowAIC", "OR
   # ESOR (Ensemble Significant pROC, low Omission Rate) - Cobos et al 2019
   # Selects models according to: pROC<=0.05; OR<=ORspecified; AICc<=2
   if(any(c("ESOR", "ESORIC") %in% mSel)){
-    mod.cobos <- x#[!is.na(x$avg.pROC.p),]
+    mod.cobos <- x #[!is.na(x$avg.pROC.p),]
     mod.cobos[is.na(mod.cobos$avg.pROC.p),"avg.pROC.p"] <- 1
     ESOR.pROC <- mod.cobos$avg.pROC.p<=0.05
     if(sum(ESOR.pROC)==0) {
-      ESOR.pROC <- mod.cobos$avg.pROC.p == min(mod.cobos$avg.pROC.p)
+      ESOR.pROC <- mod.cobos$avg.pROC.p == min(mod.cobos$avg.pROC.p, na.rm = T)
       warning("ESORIC: No model with pROC p value <= 0.05. Using model with lowest pROC.p")
     }
     ESOR.or <- mod.cobos$avg.test.or10pct<=0.1
     if(sum(ESOR.or)==0) {
-      ESOR.or <- mod.cobos$avg.test.or10pct == min(mod.cobos$avg.test.or10pct)
+      ESOR.or <- mod.cobos$avg.test.or10pct == min(mod.cobos$avg.test.or10pct, na.rm = T)
       warning("ESORIC: No model with OR <= OR criteria. Using model with lowest OR")
     }
     ESOR <- ESOR.pROC & ESOR.or
     if(sum(ESOR)==0) {
-      ESOR <- mod.cobos$avg.test.or10pct == min(mod.cobos$avg.test.or10pct[ESOR.pROC]) #mod.cobos$avg.pROC.p<=0.05 &
+      ESOR <- mod.cobos$avg.test.or10pct == min(mod.cobos$avg.test.or10pct[ESOR.pROC], na.rm = T) #mod.cobos$avg.pROC.p<=0.05 &
       warning("ESORIC: No model match pROC criteria. Using model with lowest OR from models with lowest pROC.p")
-      }
-    delta <- mod.cobos$AICc - min(mod.cobos$AICc[ESOR])
+    }
+    delta <- mod.cobos$AICc - min(mod.cobos$AICc[ESOR], na.rm = T)
     ESOR <- delta<=dAICc & delta>=0 & ESOR
     x$sel.cri[ESOR] <- sub("^\\.", "", paste(x$sel.cri[ESOR], paste0("ESORIC_", 1:sum(ESOR)), sep = "."))
   }
