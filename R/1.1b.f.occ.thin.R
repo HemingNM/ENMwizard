40671a7b70ad34c22caf8e7bfd4319d82469527d
HemingNM <neanderh@yahoo.com.br>
2021-11-10 13:59:13 -0300

    env_thin now can handle occurrences with NA in the predictor variables

--- R/1.1b.f.occ.thin.R
+++ R/1.1b.f.occ.thin.R
@@ -31,6 +31,7 @@ rescale <- function(x, x.min = NULL, x.max = NULL, new.min = 0, new.max = 1) {
 #' each variable.
 #' @export
 e_thin_algorithm <- function(data, bins=20){
+  if(sum(is.na(data))>0){ stop("data cannot contain NAs") }
   ## size of group
   grp_size <- rowSums(apply(data, 2,
                             function(x, bins){
@@ -77,8 +78,9 @@ e_thin_algorithm <- function(data, bins=20){
     gr <- grp_ids == grps[i]
     sel.rec[i] <- which(gr)[which.min(grp_centerdist[gr])]
   }
-
-  return(sort(sel.rec))
+  sel.vec <- vector("logical",nrow(data))
+  sel.vec[sel.rec] <- T
+  return(sel.vec)
 }
 
 
@@ -99,6 +101,7 @@ e_thin_algorithm <- function(data, bins=20){
 #' @param bins Number of bins to divide each environmental variable.
 #' @param file either a character string naming a file or a connection open for writing.
 #' @param plot Logical. Should results be plotted?
+#' @param verbose Logical. Should information be printed on screen?
 #'
 #' @seealso \code{\link{env_thin_b}}, \code{\link{load_env_thin_occ}}
 #'
@@ -109,6 +112,7 @@ e_thin_algorithm <- function(data, bins=20){
 #'
 #' # Download data for present
 #' dir.create("rasters")
+#' library(raster)
 #' predictors <- getData('worldclim', var='bio', res=10, path="rasters")
 #'
 #' #cor(data)
@@ -118,43 +122,66 @@ e_thin_algorithm <- function(data, bins=20){
 #' }
 #'
 #' @export
-env_thin <- function(p, predictors, long.col=NULL, lat.col=NULL, bins=20, file=NULL, plot=F){
+env_thin <- function(p, predictors, long.col=NULL, lat.col=NULL, bins=20, file=NULL, plot=F, verbose=F){
   if(class(p) %in% c("data.frame", "matrix")){
     if(is.null(lat.col) | is.null(long.col)) {
       long.col <- colnames(p)[grep("^lon$|^long$|^longitude$", colnames(p), ignore.case = T, fixed = F)][1]
       lat.col <- colnames(p)[grep("^lat$|^latitude$", colnames(p), ignore.case = T)][1]
     }
     coords <- p[,c(long.col, lat.col)]
+  } else { # if(class(p) %in% c("SpatialPoints", "SpatialPointsDataFrame")){
+    coords <- p
   }
 
   data <- raster::extract(predictors, coords)
-  selRec <- e_thin_algorithm(data, bins=bins)
+  nas <- rowSums(is.na(data))>0
+  if(sum(nas)>0) warning(paste(sum(nas), "record(s) with NA for at least one predictor"))
+  selRec <- e_thin_algorithm(data[!nas,], bins=bins)
 
   if(plot){
-    graphics::par(mfrow = c(1, 2), mar = c(4, 4, 0, 0.5))
-    plot(data[,1:2], col="gray", pch=19)
-    graphics::points(data[selRec, 1:2], col="brown", pch=19)
-    plot(p, col="gray", pch=19)
+    graphics::par(mfrow = c(1, 2), mar = c(7.2, 4, .5, .5))
+    plot(data[,1:2], col="white", pch=19)
+    graphics::points(data[!nas,][selRec, 1:2], col=grDevices::rgb(34,139,34, 100, maxColorValue=255) , pch=19)
+    graphics::points(data[!nas,][!selRec,1:2], col=grDevices::rgb(255,0,0, 130, maxColorValue=255), pch=19, cex=.6)
+    # graphics::points(data[nas,1:2], col=grDevices::rgb(255,0,0, 130, maxColorValue=255) , pch=19)
+    plot(coords, col="white", pch=19)
     # maps::map(add = T)
-    graphics::points(p[selRec,], col="brown", pch=19)
+    graphics::points(coords[!nas,][selRec,], col=grDevices::rgb(34,139,34, 100, maxColorValue=255) , pch=19)
+    graphics::points(coords[!nas,][!selRec,], col=grDevices::rgb(255,0,0, 130, maxColorValue=255), pch=19, cex=.6)
+    graphics::points(coords[nas,], col="black", pch=19, cex=.6)
+    graphics::par(mfrow = c(1, 1), mar = c(1, .5, .5, .5), xpd=TRUE)
+    graphics::legend("bottom", inset=c(.20,-.90), horiz = F, ncol=2,
+           title = "Occurrences",
+           legend=c("retained", "with NAs", "removed"), pch=19,
+           col=c(grDevices::rgb(34,139,34, 150, maxColorValue=255),
+                 "black",
+                 grDevices::rgb(255,0,0, 150, maxColorValue=255)),
+           cex=c(.6,.6,.6))
   }
-  if(!is.null(file)){
-    p$selRec <- F
-    p$selRec[selRec] <- T
-    utils::write.csv(as.data.frame(p), file = file)
-    sink(gsub(".csv", ".log.txt", file))
-    cat("\n**********************************************\n
+
+  m <- paste("\n**********************************************\n
         Environmental thinning\n
-        Original number of records:", nrow(p), "\n
-        Number of retained records:", length(selRec), "\n
+        Environmental variables used for thinning:", paste(colnames(data), collapse=", "), "\n
         Number of bins:", bins, "\n
-        Environmental variables used for thinning:", colnames(data), "\n
+        Original number of records:", nrow(p), "\n
+        Number of retained records:", sum(selRec), "\n
+        Number of records with NA for at least one variable:", sum(nas), "\n
         Thinning done on:",  as.character(Sys.time()), "\n
         File saved as:", file, "\n
         **********************************************")
+  if(verbose){
+    cat(m, "\n")
+  }
+  if(!is.null(file)){
+    p$selRec <- F
+    p$selRec[nas] <- NA
+    p$selRec[!nas][selRec] <- T
+    utils::write.csv(as.data.frame(p), file = file)
+    sink(gsub(".csv", ".log.txt", file))
+    cat(m)
     sink()
   }
-  return(coords[selRec,])
+  return(coords[!nas,][selRec,])
 }
 
 
@@ -167,13 +194,13 @@ env_thin <- function(p, predictors, long.col=NULL, lat.col=NULL, bins=20, file=N
 #' @return
 #' A data.frame, matrix, sp or sf object containing the selected records.
 #'
-#' @param predictors.lst List of \code{\link[raster]{Raster-class}} object of environmental
-#' predictor variables.
+#' @param predictors.lst A \code{\link[raster]{Raster-class}} or list of \code{\link[raster]{Raster-class}}
+#' object containing the predictor variables.
 #' @param p.lst  List of two column matrix or data.frame with point coordinates
 #' or \code{\link[sp]{SpatialPoints}} of occurrence records.
 #' @inheritParams env_thin
-#' @param bins Number of bins to divide each environmental variable.
-#' @param plot Logical. Should results be plotted?
+# #' @param bins Number of bins to divide each environmental variable.
+# #' @param plot Logical. Should results be plotted?
 #'
 #' @seealso \code{\link{env_thin}}, \code{\link{load_env_thin_occ}}
 #'
@@ -193,19 +220,19 @@ env_thin <- function(p, predictors, long.col=NULL, lat.col=NULL, bins=20, file=N
 #' }
 #'
 #' @export
-env_thin_b <- function(p.lst, predictors.lst, long.col=NULL, lat.col=NULL, bins=20, plot=F){
+env_thin_b <- function(p.lst, predictors.lst, long.col=NULL, lat.col=NULL, bins=20, plot=F, verbose=F){
   spp <- names(p.lst)
 
   if(class(predictors.lst) != "list"){
     f_thin <- function(i, p.lst, predictors.lst, long.col, lat.col, bins, spp, ...){
       file=paste0("1_sppData/occ.thinned.full/",
-                  spp[i],".occ_env_thinned.csv")
+                  spp[i],"_occ_env_thinned.csv")
       env_thin(p.lst[[i]], predictors.lst, long.col, lat.col, bins, file, ...)
     }
   } else {
     f_thin <- function(i, p.lst, predictors.lst, long.col, lat.col, bins, spp, ...){
       file=paste0("1_sppData/occ.thinned.full/",
-                  spp[i],".occ_env_thinned.csv")
+                  spp[i],"_occ_env_thinned.csv")
       env_thin(p.lst[[i]], predictors.lst[[i]], long.col, lat.col, bins, file, ...)
     }
   }
@@ -213,7 +240,7 @@ env_thin_b <- function(p.lst, predictors.lst, long.col=NULL, lat.col=NULL, bins=
   thinned_dataset_full <- lapply(1:length(p.lst), f_thin,
                                  p.lst=p.lst, predictors.lst=predictors.lst,
                                  long.col=long.col, lat.col=lat.col, bins=bins,
-                                 spp=spp)
+                                 spp=spp, plot=plot, verbose=verbose)
 
   names(thinned_dataset_full) <- spp
 
