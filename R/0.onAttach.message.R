0e1a918718ce909ed0cf664b7aead7f1b24e7fbc
Neander Marcel Heming <neanderh@yahoo.com.br>
2022-03-29 09:00:12 -0300

    changed function for Startup Message

--- R/0.onAttach.message.R
+++ R/0.onAttach.message.R
@@ -1,6 +1,3 @@
-.packageStartupMessage <- function(libname = find.package("ENMwizard"), pkgname = "ENMwizard") {
-  packageStartupMessage(cat("    Welcome to ENMwizard \n  To cite this package use: citation('ENMwizard'). \n  Please, remember to cite other packages that ENMwizard depends on: 'ENMeval', 'spThin', 'raster'"))
+.onAttach <- function(libname = find.package("ENMwizard"), pkgname = "ENMwizard") {
+  packageStartupMessage(cat("    Welcome to ENMwizard \n  To cite this package use: citation('ENMwizard'). \n  Please, remember to cite other packages that ENMwizard depends on: 'raster', 'dismo', 'ENMeval', 'spThin'"))
 }
-
-
-
