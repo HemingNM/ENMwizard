b8f30a1e7c255ce43c2f45541e418f06879dbc74
Neander Marcel Heming <neanderh@yahoo.com.br>
2023-06-12 09:36:24 -0300

    added error message for ENMeval versions >= 1.0

--- R/3.f.tuning.R
+++ R/3.f.tuning.R
@@ -37,6 +37,14 @@ ENMevaluate_b <- function(occ.l, a.calib.l, bg.coords.l = NULL,
                           rasterPreds = TRUE, parallel = FALSE, numCores = NULL,
                           progbar = TRUE, updateProgress = FALSE,
                           resultsOnly = F, ...){
+
+  if(packageVersion("ENMeval") >= "1.0"){stop('ENMeval version >= 1.0 is not yet supported.
+    Please install an older version using:
+         library(devtools)
+         detach("package:ENMeval", unload=TRUE)
+         install_version("ENMeval", version = "0.3.1")')
+  }
+
   test.args <- function(x, y, obj=""){
     if(!is.null(x)) {
       if(length(x)==1){
